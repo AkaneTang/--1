97d9eb14100514294305c22d01193918e5a1fedf
Akane <865442122@qq.com>
2021-01-29 00:12:19 +0800

    update build

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 let html = document.querySelector("#html");
 let style = document.querySelector('#style')
 
-let string = `/* 你好，我叫小芳
+let string = `/* 你好，我叫Akane
  * 接下来我要演示一下我的前端功底
  * 首先我要准备一个div
  */
@@ -11,16 +11,15 @@ let string = `/* 你好，我叫小芳
     height: 200px;
 }
 /* 接下来我把div变成一个八卦图
- * 注意看好了
- * 首先把div变成一个圆
+ * 首先要有一个圆
  */
  #div1{
      border-radius: 50%;
      box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
      border: none;
  }
- /* 八卦是阴阳形成的
- * 一黑一白
+/* 太极生两仪
+ * 两仪生八卦
  */
 #div1{
     background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
